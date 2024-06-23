00b45d74bcb655971fa79b4082e059c5cb793b69
imranulmean <imranulhasan73@gmail.com>
2024-06-24 00:14:04 +0600

    Edit Channel

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -5,6 +5,7 @@ import { app } from '@/firebase';
 import {
   addDoc,
   collection,
+  getDocs,
   getFirestore,
   onSnapshot,
   query,
@@ -18,6 +19,7 @@ const TestPage = () => {
   const [channelName, setChannelName] = useState('');
   const [channelServers, setChannelServers] = useState('');
   const [selectedCategory, setSelectedCategory] = useState({ id: '', name: '' });
+  const [editChannels, seteditChannels]= useState([]);
 
   const db = getFirestore(app);
 
@@ -50,9 +52,24 @@ const TestPage = () => {
     alert("Channel Added");
   }
 
+  async function fetchChannels(){
+    console.log("fetching channels");
+    console.log(selectedCategory)
+    const q= query(collection(db, 'channelCategory',selectedCategory.id,'channel'))
+    const querySnapshot = await getDocs(q);
+    let channels=[];
+    for (const doc of querySnapshot.docs) {
+      const channelData = doc.data();
+      const channelId = doc.id;
+      channels.push({ id: channelId, channelName: channelData.channelName, channelServers:channelData.channelServers });
+    }
+    console.log(channels);
+    seteditChannels(channels);
+  }
+
   return (
     <div>
-      <label htmlFor="channelCategory" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Create Channel Category</label>
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Create Channel Category</h5>
       <input 
         type="text" 
         className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" 
@@ -68,8 +85,7 @@ const TestPage = () => {
         Create Channel Category
       </button>
 
-      <label htmlFor="channelName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Create Channel</label>
-
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Create Channel</h5>
       <label htmlFor="categories" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Channel Category</label>
       <select 
         id="categories" 
@@ -113,6 +129,71 @@ const TestPage = () => {
       >
         Create Channel
       </button>
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Edit Channel</h5>
+      <select 
+        id="categories" 
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-4"
+        value={selectedCategory.id}
+        onChange={(e) => {
+          const selectedId = e.target.value;
+          const selectedName = channelCategories.find(cat => cat.id === selectedId)?.channelCategory || '';
+          setSelectedCategory({ id: selectedId, name: selectedName });
+        }}
+      >
+        <option value="" disabled>Select a category</option>
+        {channelCategories.map(category => (
+          <option key={category.id} value={category.id}>
+            {category.channelCategory}
+          </option>
+        ))}
+      </select>
+      <button 
+        onClick={fetchChannels} 
+        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >Fetch Channels</button>
+      <div class="relative overflow-x-auto">
+          <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+              <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                      <th scope="col" class="px-6 py-3">
+                          Channel Name
+                      </th>
+                      <th scope="col" class="px-6 py-3">
+                        channelServers
+                      </th>
+                      <th scope="col" class="px-6 py-3">
+                          Edit
+                      </th>
+                      <th scope="col" class="px-6 py-3">
+                          Delete
+                      </th>
+                  </tr>
+              </thead>
+              <tbody>
+                {
+                  editChannels.map(c=>{
+                  return(
+                      <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                          <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                              {c.channelName}
+                          </th>
+                          <td class="px-6 py-4">
+                              {c.channelServers}
+                          </td>
+                          <td class="px-6 py-4">
+                              Edit
+                          </td>
+                          <td class="px-6 py-4">
+                              Delete
+                          </td>
+                      </tr>  
+                    )
+                  })
+                }
+
+              </tbody>
+          </table>
+      </div>      
     </div>
   );
 };
